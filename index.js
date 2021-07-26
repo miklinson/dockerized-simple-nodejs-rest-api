8129809c832bff9d0f8eba21fadca528ba7e2b0d
Mik Linson <miklinson@gmail.com>
2021-07-26 21:10:27 +0800

    feat: Add base path /

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const menu = require('./routes/menu')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json({"message": "dockerized food menu app base path"})
+})
 app.use('/menu', menu)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
