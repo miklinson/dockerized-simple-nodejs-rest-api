35298074683d90cf0939df4a25f359bbfad7c1e5
Mik Linson <miklinson@gmail.com>
2021-07-26 10:19:15 +0800

    feat: Add route to express server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const menu = require('./routes/menu')
 
 const app = express()
 
+app.use('/menu', menu)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
