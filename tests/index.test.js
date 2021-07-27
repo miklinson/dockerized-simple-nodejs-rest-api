49e69398816dbe506f663b360df7b9453c7a37ab
Mik Linson <miklinson@gmail.com>
2021-07-27 11:14:54 +0800

    chore: Add test for / endpoint

--- tests/index.test.js
+++ tests/index.test.js
@@ -24,3 +24,12 @@ test("GET /menu/steak", async ()=>{
             expect(response.body.steak[0].doneness).toBeDefined()
         })
 })
+
+test("GET /", async ()=>{
+    await supertest(app).get("/")
+        .expect(200)
+        .then((response)=>{
+            expect(response.body.message).toBeDefined()
+            expect(response.body.message).toBe("dockerized food menu app base path")
+        })
+})
