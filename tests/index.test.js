ee67c44c42076202eee5dcab631f75befd73ef1c
Mik Linson <miklinson@gmail.com>
2021-07-27 11:10:02 +0800

    chore: Add simple test, using jest and supertest

---
+++ tests/index.test.js
@@ -0,0 +1,26 @@
+const app = require("../index");
+const supertest = require("supertest");
+const { response } = require("express");
+
+test("GET /menu", async ()=>{
+    await supertest(app).get("/menu")
+        .expect(200)
+        .then((response)=>{
+            //Check data
+            expect(response.body.menu).toBeDefined();
+            expect(response.body.menu[0].name).toBeDefined();
+            expect(response.body.menu[0].name).toBe('Steak');
+        })
+});
+
+test("GET /menu/steak", async ()=>{
+    await supertest(app).get("/menu/steak")
+        .expect(200)
+        .then((response)=>{
+            expect(response.body.steak).toBeDefined()
+            expect(response.body.steak[0]).toBeDefined()
+            expect(response.body.steak[0].name).toBeDefined()
+            expect(response.body.steak[0].cook_method).toBeDefined()
+            expect(response.body.steak[0].doneness).toBeDefined()
+        })
+})
